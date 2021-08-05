7960a774622ef2dc1e0271b04ff7f34cb86bdc0a
Samuel Prevost <samuel.prevost@pm.me>
2021-08-05 20:28:42 +0200

    Add unit tests for composite calls, e.g. Numpy + Pytorch

--- tests/test_pshape.py
+++ tests/test_pshape.py
@@ -29,7 +29,37 @@ class TestPshape(unittest.TestCase):
 
     @unittest.skipUnless(TF_ENABLED, "tensorflow not installed")
     def test_tensorflow(self):
-        pshape(tf.eye(4), tf.reshape(tf.range(10), (5,2,1)), heading=True)
+        pshape(tf.eye(4), tf.reshape(tf.range(10), (5, 2, 1)), heading=True)
+
+    @unittest.skipUnless(PYTORCH_ENABLED, "torch not installed")
+    def test_pytorch_np(self):
+        pshape(np.eye(4), torch.arange(10).view(5, 2, 1), heading=True)
+
+    @unittest.skipUnless(TF_ENABLED, "tensorflow not installed")
+    def test_tensorflow_np(self):
+        pshape(np.eye(4), tf.reshape(tf.range(10), (5, 2, 1)), heading=True)
+
+    @unittest.skipUnless(
+        TF_ENABLED and PYTORCH_ENABLED, "pytorch and tensorflow need to be installed"
+    )
+    def test_tensorflow_pytorch(self):
+        pshape(
+            torch.arange(10).view(5, 2, 1),
+            tf.reshape(tf.range(10), (5, 2, 1)),
+            heading=True,
+        )
+
+    @unittest.skipUnless(
+        TF_ENABLED and PYTORCH_ENABLED, "pytorch and tensorflow need to be installed"
+    )
+    def test_tensorflow_pytorch_np(self):
+        pshape(
+            np.arange(10).reshape(5, 2, 1),
+            torch.arange(10).view(5, 2, 1),
+            tf.reshape(tf.range(10), (5, 2, 1)),
+            heading=True,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
