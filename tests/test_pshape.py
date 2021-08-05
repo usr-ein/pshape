292de451cfab75c67219e3e0f4083d354d728970
Samuel Prevost <samuel.prevost@pm.me>
2021-08-05 19:48:09 +0200

    Add basic unit testing

---
+++ tests/test_pshape.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module doc"""
+import unittest
+from pshape import pshape
+import numpy as np
+
+try:
+    import torch
+
+    PYTORCH_ENABLED = True
+except ImportError:
+    PYTORCH_ENABLED = False
+
+try:
+    import tensorflow as tf
+
+    TF_ENABLED = True
+except ImportError:
+    TF_ENABLED = False
+
+
+class TestPshape(unittest.TestCase):
+    def test_doesnt_crash(self):
+        pshape(np.eye(4), np.arange(10).reshape(5, 2, 1), heading=True)
+
+    @unittest.skipUnless(PYTORCH_ENABLED, "torch not installed")
+    def test_pytorch(self):
+        pshape(torch.eye(4), torch.arange(10).view(5, 2, 1), heading=True)
+
+    @unittest.skipUnless(TF_ENABLED, "tensorflow not installed")
+    def test_tensorflow(self):
+        pshape(tf.eye(4), tf.reshape(tf.range(10), (5,2,1)), heading=True)
+
+if __name__ == "__main__":
+    unittest.main()
